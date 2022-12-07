3993f888c20f8183f7472c0669e3744d28bf35a8
devonfox <devonhouston.fox@gmail.com>
2022-12-07 15:26:17 -0800

    init commit (day2 complete)

---
+++ aoc_2022_rust/src/day1.rs
@@ -0,0 +1,47 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<Vec<u32>> {
+    input
+        .split("\n\n")
+        .map(|group| group.lines().map(|line| line.parse().unwrap()).collect())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
+    input.iter().map(|group| group.iter().sum()).max().unwrap()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
+    let mut grouped: Vec<u32> = input
+        .clone()
+        .iter()
+        .map(|group| group.iter().sum())
+        .collect();
+
+    grouped.sort();
+    grouped.reverse();
+    grouped[0] + grouped[1] + grouped[2]
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::input_generator;
+
+    use super::part1;
+
+    #[test]
+    fn parses() {
+        let input = "100\n200\n\n100\n100\n";
+        assert_eq!(
+            input_generator(&input),
+            vec![vec![100, 200], vec![100, 100]]
+        );
+    }
+
+    #[test]
+    fn finds_max() {
+        let input: Vec<Vec<u32>> = vec![vec![100, 200], vec![100, 100]];
+        assert_eq!(part1(&input), 300);
+    }
+}
---
+++ aoc_2022_rust/src/day2.rs
@@ -0,0 +1,76 @@
+#[aoc_generator(day2)]
+pub fn input_generator(input: &str) -> Vec<(char, char)> {
+    input
+        .split("\n")
+        .map(|x| (x.chars().nth(0).unwrap(), x.chars().nth(2).unwrap()))
+        .collect()
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &Vec<(char, char)>) -> u32 {
+    input
+        .iter()
+        .map(|x| {
+            let (u, v) = x;
+            match (u, v) {
+                ('A', 'Y') => 2 + 6,
+                ('A', 'Z') => 3 + 0,
+                ('B', 'X') => 1 + 0,
+                ('B', 'Z') => 3 + 6,
+                ('C', 'X') => 1 + 6,
+                ('C', 'Y') => 2 + 0,
+                (_, _) => match v {
+                    'X' => 4,
+                    'Y' => 5,
+                    'Z' => 6,
+                    _ => 0,
+                },
+            }
+        })
+        .sum()
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &Vec<(char, char)>) -> u32 {
+    input
+        .iter()
+        .map(|x| {
+            let (u, v) = x;
+            match v {
+                'X' => match u {
+                    'A' => 3,
+                    'B' => 1,
+                    'C' => 2,
+                    _ => 0,
+                },
+                'Y' => match u {
+                    'A' => 3 + 1,
+                    'B' => 3 + 2,
+                    'C' => 3 + 3,
+                    _ => 0,
+                },
+                'Z' => match u {
+                    'A' => 6 + 2,
+                    'B' => 6 + 3,
+                    'C' => 6 + 1,
+                    _ => 0,
+                },
+                _ => 0,
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day2::input_generator;
+
+    #[test]
+    fn parses() {
+        let input = "A Y\nB X\nC Z";
+        assert_eq!(
+            input_generator(input),
+            vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')]
+        );
+    }
+}
