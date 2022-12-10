635494d498ee897b7b19f761ccf6bbb9c0024480
devonfox <devonhouston.fox@gmail.com>
2022-12-09 16:41:14 -0800

    finished day3!

--- aoc_2022_rust/src/day3.rs
+++ aoc_2022_rust/src/day3.rs
@@ -23,7 +23,7 @@ pub fn part1(input: &Vec<String>) -> u32 {
                     }
                 }
             }
-            
+
             // found this here
             // https://github.com/simonw/advent-of-code-2022-in-rust/issues/4
             let priority = match code {
@@ -37,11 +37,40 @@ pub fn part1(input: &Vec<String>) -> u32 {
         .sum()
 }
 
+#[aoc(day3, part2)]
+pub fn part2(input: &Vec<String>) -> u32 {
+    let threes = input.chunks(3);
+    let mut sum: u32 = 0;
+    for t in threes {
+        let val = find_value(t);
+        sum += val;
+    }
+    sum
+}
+
+pub fn find_value(input: &[String]) -> u32 {
+    let first = input[0].as_str();
+    let second = input[1].as_str();
+    let third = input[2].as_str();
+
+    let priority = first
+        .chars()
+        .find(|c| second.contains(*c) && third.contains(*c))
+        .map(|c| match c {
+            'a'..='z' => c as u32 - 'a' as u32 + 1,
+            'A'..='Z' => c as u32 - 'A' as u32 + 27,
+            _ => 0,
+        })
+        .unwrap_or(0);
+
+    priority
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day3::input_generator;
 
-    use super::part1;
+    use super::{part1, part2};
 
     #[test]
     pub fn parses() {
@@ -58,10 +87,18 @@ mod tests {
     }
 
     #[test]
-    pub fn totals() {
-        let initial_input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
+    pub fn totals_part1() {
+        let initial_input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
         let input = input_generator(initial_input);
         let answer = part1(&input);
-        assert_eq!(answer, 118);
+        assert_eq!(answer, 157);
+    }
+
+    #[test]
+    pub fn totals_part2() {
+        let initial_input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        let input = input_generator(initial_input);
+        let answer = part2(&input);
+        assert_eq!(answer, 70);
     }
 }
