2e5ac059accdd626e8c7e72d6d11c2ba03fc0473
devonfox <devonhouston.fox@gmail.com>
2022-12-11 22:54:41 -0800

    day 4, part 2 complete

--- aoc_2022_rust/src/day4.rs
+++ aoc_2022_rust/src/day4.rs
@@ -22,17 +22,37 @@ pub fn part1(input: &Vec<(u32, u32, u32, u32)>) -> u32 {
         .sum()
 }
 
+#[aoc(day4, part2)]
+pub fn part2(input: &Vec<(u32, u32, u32, u32)>) -> u32 {
+    input
+        .iter()
+        .map(
+            |(a, b, c, d)| match (c < b && a <= d) || (a < d && b >= c) || (a == c || b == d) {
+                true => 1,
+                false => 0,
+            },
+        )
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day4::input_generator;
 
-    use super::part1;
+    use super::{part1, part2};
 
     #[test]
     pub fn totals_part1() {
         let input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8";
         let parsed = input_generator(input);
         assert_eq!(part1(&parsed), 2);
+    }
+
+    #[test]
+    pub fn totals_part2() {
+        let input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8";
+        let parsed = input_generator(input);
+        assert_eq!(part2(&parsed), 4);
     }
 
     #[test]
